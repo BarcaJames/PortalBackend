20b16ce5c9b280aa5a3db86a861da309c0685269
Jamie James <49459617+BarcaJames@users.noreply.github.com>
2021-11-20 21:26:59 -0500

    Add superuser to system

--- src/main/java/com/portal/PortalApiApplication.java
+++ src/main/java/com/portal/PortalApiApplication.java
@@ -1,11 +1,18 @@
 package com.portal;
 
+import com.portal.domain.User;
+import com.portal.repository.UserRepository;
+import lombok.extern.slf4j.Slf4j;
+import net.bytebuddy.utility.RandomString;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.web.servlet.error.ErrorMvcAutoConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CorsFilter;
@@ -13,17 +20,47 @@ import org.springframework.web.filter.CorsFilter;
 import java.io.File;
 import java.util.Arrays;
 import java.util.Collections;
+import java.util.Date;
 
 import static com.portal.constant.FileConstant.USER_FOLDER;
+import static com.portal.enumeration.Role.ROLE_SUPER_ADMIN;
 
 @SpringBootApplication
 
 public class PortalApiApplication {
 
+    @Value("${SuperAdmin.password}")
+    private String superAdminPassword;
+
     public static void main(String[] args) {
         SpringApplication.run(PortalApiApplication.class, args);
         // Add folder to user home on whatever server this is running on
         new File(USER_FOLDER).mkdirs();
+    }
+
+    @Bean
+    public CommandLineRunner demoData(UserRepository repo) {
+
+        return args -> {
+            User user;
+            user = repo.findUserByUsername("Jamie");
+            if(user == null) {
+                user = new User();
+                user.setFirstName("Jamie");
+                user.setLastName("James");
+                user.setUsername("Jamie");
+                user.setEmail("jamie1k_86@yahoo.com");
+                user.setRole("ROLE_SUPER_ADMIN");
+                user.setUserId(RandomString.make(10));
+                user.setJoinDate(new Date());
+                user.setActive(true);
+                user.setNotLocked(true);
+                user.setPassword(bCryptPasswordEncoder().encode(superAdminPassword));
+                user.setProfileImageUrl("http://localhost:8080/user/image/profile/Jamie");
+                user.setAuthorities(ROLE_SUPER_ADMIN.getAuthorities());
+                repo.save(user);
+            }
+        };
     }
 
     @Bean
@@ -46,4 +83,5 @@ public class PortalApiApplication {
     public BCryptPasswordEncoder bCryptPasswordEncoder(){
         return new BCryptPasswordEncoder();
     }
+
 }
