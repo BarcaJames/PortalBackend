0dcc5f83bd97dc917de585acc8649484cff5db5b
Jamie James <49459617+BarcaJames@users.noreply.github.com>
2021-11-21 01:50:31 -0500

    Added schedule to drop database and seed it with default data midnight everyday

---
+++ src/main/java/com/portal/configuration/ScheduleConfiguration.java
@@ -0,0 +1,56 @@
+package com.portal.configuration;
+
+import com.portal.domain.User;
+import com.portal.enumeration.Role;
+import com.portal.repository.UserRepository;
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import net.bytebuddy.utility.RandomString;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+import java.util.Date;
+
+import static com.portal.enumeration.Role.*;
+
+@Configuration
+@EnableScheduling
+@Slf4j
+@AllArgsConstructor
+public class ScheduleConfiguration {
+
+    private final UserRepository userRepository;
+    private final BCryptPasswordEncoder bCryptPasswordEncoder;
+
+    @Scheduled(cron = "0 0 0 * * *", zone = "GMT-5")
+    public void cleanUp(){
+        userRepository.deleteAll();
+
+        userRepository.save(buildUser("Jamie", "James", "Jamie", ROLE_SUPER_ADMIN));
+        userRepository.save(buildUser("Jane", "Brown", "Jane", ROLE_ADMIN));
+        userRepository.save(buildUser("Kimberly", "Davis", "Kimmy", ROLE_MANAGER));
+        userRepository.save(buildUser("Pam", "Green", "Pam", ROLE_HR));
+        userRepository.save(buildUser("John", "Brown", "Johnny", ROLE_USER));
+
+    }
+
+    private User buildUser(String firstName, String lastName, String username, Role role){
+        User user = new User();
+        user.setFirstName(firstName);
+        user.setLastName(lastName);
+        user.setUsername(username);
+        user.setEmail(firstName+"@portal.com");
+        user.setRole(role.name());
+        user.setUserId(RandomString.make(10));
+        user.setJoinDate(new Date());
+        user.setActive(true);
+        user.setNotLocked(true);
+        user.setPassword(bCryptPasswordEncoder.encode("password"));
+        user.setProfileImageUrl("http://localhost:8080/user/image/profile/"+ username);
+        user.setAuthorities(role.getAuthorities());
+
+        return user;
+    }
+}
