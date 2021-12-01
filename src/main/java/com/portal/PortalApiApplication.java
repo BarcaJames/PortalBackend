e3baa890aace0ec0effe861bcee14c6855f6bb3a
Jamie James <49459617+BarcaJames@users.noreply.github.com>
2021-11-30 22:53:29 -0500

    Added client url

--- src/main/java/com/portal/PortalApiApplication.java
+++ src/main/java/com/portal/PortalApiApplication.java
@@ -3,6 +3,7 @@ package com.portal;
 import com.portal.domain.Users;
 import com.portal.repository.UserRepository;
 import net.bytebuddy.utility.RandomString;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -21,6 +22,8 @@ import static com.portal.enumeration.Role.ROLE_SUPER_ADMIN;
 @SpringBootApplication
 
 public class PortalApiApplication {
+    @Value( "${clientUrl}" )
+    private String clientUrl;
 
     public static void main(String[] args) {
         SpringApplication.run(PortalApiApplication.class, args);
@@ -52,10 +55,11 @@ public class PortalApiApplication {
 
     @Bean
     public CorsFilter corsFilter() {
+
         UrlBasedCorsConfigurationSource urlBasedCorsConfigurationSource = new UrlBasedCorsConfigurationSource();
         CorsConfiguration corsConfiguration = new CorsConfiguration();
         corsConfiguration.setAllowCredentials(true);
-        corsConfiguration.setAllowedOrigins(Collections.singletonList("http://localhost:3000"));
+        corsConfiguration.setAllowedOrigins(Collections.singletonList(clientUrl));
         corsConfiguration.setAllowedHeaders(Arrays.asList("Origin", "Access-Control-Allow-Origin", "Content-Type",
                 "Accept", "Jwt-Token", "Authorization", "Origin, Accept", "X-Requested-With",
                 "Access-Control-Request-Method", "Access-Control-Request-Headers"));
